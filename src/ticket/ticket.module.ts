954e56f84bddec9b3fd017b00c5c2eee62f64f2b
Jerick <ustpaa.villanueva@ustp.edu.ph>
2026-05-26 12:07:14 +0800

    fix: Add Employee entity to TicketModule imports for EmployeeRepository injection

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketService } from './ticket.service';
 import { TicketController } from './ticket.controller';
 import { Ticket } from '../entities/ticket.entity';
+import { Employee } from '../entities/employee.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Ticket])],
+  imports: [TypeOrmModule.forFeature([Ticket, Employee])],
   controllers: [TicketController],
   providers: [TicketService],
   exports: [TicketService],
